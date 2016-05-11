23d82e5b34ed2d0d95789b16af14b2faf9659b5c
kkang1119 <kkang1119@gmail.com>
2016-05-11 15:51:41 -0400

    Homework 1 Part 2 Complete

--- Homework 4 Part1/src/Week10/Main.java
+++ Homework 4 Part1/src/Week10/Main.java
@@ -5,21 +5,22 @@ public class Main{
         System.out.println("Default constructor");
     }
     public Main(String str){
-        //It will call a default constrctor
-        System.out.println("Parametrized constructor with single param");
+        //It will call a default constructor
+        System.out.println(str);
     }
     public Main(String str, int num){
         //It will call the constructor with String argument
-
-        System.out.println("Parametrized constructor with double args");
+        System.out.println(str + " and " + num);
     }
     public Main(int num1, int num2, int num3){
         // It will call the constructor with (String, integer) arguments
-
-        System.out.println("Parametrized constructor with three args");
+        System.out.println(num1 + " " + num2 + " " + num3);
     }
     public static void main(String args[]){
         //Creating an object using Constructor with 3 int arguments
-
+        Main constructor = new Main();
+        Main constructor1 = new Main("Parametrized constructor with single param");
+        Main constructor2 = new Main("Parametrized constructor with single param", 147);
+        Main constructor3 = new Main(147, 157, 167);
     }
 }
